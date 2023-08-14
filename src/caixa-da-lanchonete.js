b6a326daa88c8360fbbca70258be899da9055c12
Hugo Diêgo <hugodiego.898@gmail.com>
2023-08-14 16:07:21 -0300

    Desafio DB Server

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -14,85 +14,107 @@ const CARDAPIO = {
     preco: 2,
     extraDo: "sanduiche",
   }
-}
+};
 
 const FORMA_DE_PAGAMENTO = {
   dinheiro: "dinheiro",
   debito: "debito",
   credito: "credito",
-}
+};
 
-const DESCONTO_DINHEIRO = 0.05
-const TAXA_CREDITO = 0.03
+const DESCONTO_DINHEIRO = 0.05;
+const TAXA_CREDITO = 0.03;
 
 class CaixaDaLanchonete {
+  constructor() {
+    this.pedido = {};
+    this.erros = [];
+  }
+
+  adicionarErro(mensagem) {
+    this.erros.push(mensagem);
+  }
+
+  validarFormaDePagamento(metodoDePagamento) {
+    if (!FORMA_DE_PAGAMENTO[metodoDePagamento]) {
+      this.adicionarErro("Forma de pagamento inválida!");
+    }
+  }
+
+  validarQuantidade(quantidade, produto) {
+    if (quantidade <= 0) {
+      this.adicionarErro(`Quantidade de ${produto} inválida: ${quantidade}`);
+    }
+  }
+
+  validarItem(item) {
+    const [produto, quantidade] = item.trim().toLowerCase().split(',');
+
+    if (!(produto in CARDAPIO)) {
+      this.adicionarErro(`${produto}: Item inválido ou fora do cardápio!`);
+    }
+
+    this.validarQuantidade(quantidade, produto);
+
+    this.pedido[produto] = quantidade;
+
+    const extraDO = CARDAPIO[produto]?.extraDO
+    if (extraDO && !this.itensIncluemProduto(extraDO)) {
+      this.adicionarErro(`Você só pode adicionar ${produto} como extra se houver ${CARDAPIO[produto]?.extraDo} como principal!`);
+    }
+  }
+
+  itensIncluemProduto(produto) {
+    return Object.keys(this.pedido).includes(produto);
+  }
 
   calcularValorDaCompra(metodoDePagamento, itens = []) {
-      let pedido = {}
-      const erros = []
-      
-      if (!FORMA_DE_PAGAMENTO[metodoDePagamento]){
-        return "Forma de pagamento inválida!"
-      }
-      if (itens.length <= 0) {
-        return "Não há itens no carrinho de compra!"
-      }
-      
-      itens.forEach((item, _index, array) => {
-        const [produto, quantidade] = item.trim().toLowerCase().split(',')
+    this.erros = [];
+    this.pedido = {};
 
-        if (!(produto in CARDAPIO)) {
-          erros.push(`${produto}: Item inválido ou fora do cardápio!`)
-        }
-        if (quantidade <= 0 ) {
-          erros.push(`Quantidade de ${produto} inválida: ${quantidade}`)
-        }
+    this.validarFormaDePagamento(metodoDePagamento);
 
-        pedido[produto] = quantidade
+    if (itens.length <= 0) {
+      this.adicionarErro("Não há itens no carrinho de compra!");
+    } else {
+      itens.forEach(item => this.validarItem(item));
+    }
 
-        if (
-          CARDAPIO[produto]?.extraDo && !array.some((i) => i.split(',')[0] === CARDAPIO[produto]?.extraDo) 
-        ) {
-          erros.push(`Você ira poder adicionar ${produto} como extra, se houver ${CARDAPIO[produto]?.extraDo} como principal!`)
-        }
-      })
+    if (this.erros.length > 0) {
+      return this.erros.join('\n');
+    } else {
+      const valorTotal = Object.entries(this.pedido).reduce((acumulador, [produto, quantidade]) => {
+        const precoProduto = (CARDAPIO[produto]?.preco ?? CARDAPIO[produto]) * quantidade;
+        return acumulador + precoProduto;
+      }, 0);
 
-      if (erros.length > 0){
-        return erros.join('\n')
-      }else {
-        const valorTotal = Object.entries(pedido).reduce((acumulador, [produto, quantidade]) => {
-          const precoProduto = (CARDAPIO[produto]?.preco ?? CARDAPIO[produto]) * quantidade
-          return acumulador + precoProduto
-        }, 0)
-
-        return this.calcularDescontoOuTaxa(metodoDePagamento, valorTotal)
-      }
+      return this.calcularDescontoOuTaxa(metodoDePagamento, valorTotal);
+    }
   }
+
   calcularDescontoOuTaxa(metodoDePagamento, valorTotal) {
     const format = (number) => {
-      number = number.toFixed(2).replace('.',',')
-      return `R$ ${number}`
-    }
+      number = number.toFixed(2).replace('.', ',');
+      return `R$ ${number}`;
+    };
+
     switch (metodoDePagamento) {
       case FORMA_DE_PAGAMENTO.credito: {
-        const taxa = valorTotal * TAXA_CREDITO
-        return format(valorTotal + taxa)
+        const taxa = valorTotal * TAXA_CREDITO;
+        return format(valorTotal + taxa);
       }
 
       case FORMA_DE_PAGAMENTO.dinheiro: {
-        const desconto = valorTotal * DESCONTO_DINHEIRO
-        return format(valorTotal - desconto)
+        const desconto = valorTotal * DESCONTO_DINHEIRO;
+        return format(valorTotal - desconto);
       }
 
       case FORMA_DE_PAGAMENTO.debito: {
-        return format(valorTotal)
+        return format(valorTotal);
       }
     }
   }
 }
 
-let caixaLanchonete = new CaixaDaLanchonete().calcularValorDaCompra("debito", ['cafe,1', 'queijo,1'])
-
-console.log(caixaLanchonete)
-
-export { CaixaDaLanchonete };
+const caixaLanchonete = new CaixaDaLanchonete().calcularValorDaCompra("debito", ['cafe,1', 'queijo,1']);
+console.log(caixaLanchonete);
